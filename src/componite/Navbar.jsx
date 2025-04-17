0e70f1545ef2f58541c675a9e4b06dab9b15d163
kaushalsharma97299@gmail.com <115640134+kaushal97299@users.noreply.github.com>
2025-04-18 00:50:07 +0530

    portfolio resum link1

--- src/componite/Navbar.jsx
+++ src/componite/Navbar.jsx
@@ -44,7 +44,7 @@ const Navbar = () => {
         <li className='li1'><Link to="/Skills" onClick={closeMenu}>Skills</Link></li>
         <li className='li1'><Link to="#contact" onClick={closeMenu}>Contact</Link></li>
         <li className='li2'>
-          <Link to="Kaushal Resume.pdf" download className="resume-btn" onClick={closeMenu}>
+          <Link to="/public/Kaushal Resume.pdf" download className="resume-btn" onClick={closeMenu}>
             Download Resume
           </Link>
         </li>
